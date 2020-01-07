72dc9549cdec13216cdb86c738115087d4e4e0c7
Denis <lstelmah29@gmail.com>
2020-01-07 17:07:15 +0200

    Controller Coments

--- SocialNetworkAPI/API/Controllers/AdminController.cs
+++ SocialNetworkAPI/API/Controllers/AdminController.cs
@@ -21,6 +21,11 @@ namespace api.Controllers
             _photoService = photoService;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userParams">Params for Pagiantion</param>
+        /// <returns>Ok with User</returns>
         [Authorize(Roles = "Admin")]
         [HttpGet("users/roles")]
         public async Task<IActionResult> GetUserWithRole([FromQuery]UserParams userParams)
@@ -34,6 +39,15 @@ namespace api.Controllers
             return Ok(usersForReturn);
         }
 
+        /// <summary>
+        /// Edit Roles of User
+        /// </summary>
+        /// <param name="userName">Name of User To edit</param>
+        /// <param name="roleEditDTO">List Of Rooles too edit</param>
+        /// <returns>
+        ///     Ok with roles - if success 
+        ///     BadRequest - if Fail tto Change Roles
+        /// </returns>
         [Authorize(Roles = "Admin")]
         [HttpPost("editRoles/{userName}")]
         public async Task<IActionResult> EditRoles(string userName, RoleEditDTO roleEditDTO)
@@ -47,6 +61,14 @@ namespace api.Controllers
             return Ok(roles);
         }
 
+        /// <summary>
+        /// Approve Photo
+        /// </summary>
+        /// <param name="photoId">Id of photo to approve</param>
+        /// <returns> 
+        ///     Ok with photo, wich was approved
+        ///     BadBadRequest if Photo doesn't exsist
+        /// </returns>
         [Authorize(Roles = "Moderator, Admin")]
         [HttpPut("photos/{photoId}")]
         public async Task<IActionResult> ApprovePhoto(int photoId)
@@ -60,6 +82,14 @@ namespace api.Controllers
             return Ok(photo);
         }
 
+        /// <summary>
+        /// Delete Photo
+        /// </summary>
+        /// <param name="photoId">Id of photo to delete</param>
+        /// <returns>
+        ///     Ok - if phto is deleted
+        ///     BadRequest - if Photo doesn't exsist
+        /// </returns>
         [Authorize(Roles = "Moderator, Admin")]
         [HttpDelete("photos/{photoId}")]
         public async Task<IActionResult> DeletePhoto(int photoId)
@@ -71,7 +101,11 @@ namespace api.Controllers
             return BadRequest("Photo doesn't exsist");
         }
 
-
+        /// <summary>
+        /// api/admin/photos/moderation
+        /// Get All photos, with is not approved 
+        /// </summary>
+        /// <returns>Unmodarate Photos</returns>
         [Authorize(Roles = "Moderator")]
         [HttpGet("photos/moderation")]
         public async Task<IActionResult> GetPhotosForModerator()
